df1fe13a4422fe404be4d5548b31b92ca1ae1100
Pratham Jain <83359099+iprathamjain@users.noreply.github.com>
2022-07-20 12:55:41 +0530

    Time: 254 ms (75.74%), Space: 48.1 MB (70.32%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,32 +1,65 @@
+// class Solution {
+//     int isSubSeq(string& a, string b) {
+        
+//         int i = 0, j = 0;
+//         int m = a.length(), n = b.length();
+        
+//         while (i < m && j < n) {
+//             if (a[i] == b[j]) {
+//                 i++; j++;
+//             } else {
+//                 i++;
+//             }
+//         }
+        
+//         return (j == n);
+//     }
+// public:
+//     int numMatchingSubseq(string S, vector<string>& words) {
+        
+//         int c = 0;
+//         unordered_map<string, int>mp;
+//         for (int i = 0; i < words.size(); i++)
+//             mp[words[i]]++;
+            
+//         for (auto it = mp.begin(); it != mp.end(); it++) {
+//             if (isSubSeq(S, it->first))
+//                 c += it->second;
+//         }
+        
+//         return c;
+//     }
+    
+    
+// };
+
+
 class Solution {
-    int isSubSeq(string& a, string b) {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        vector<vector<int>> charMap(26);
+        int n = s.length();
         
-        int i = 0, j = 0;
-        int m = a.length(), n = b.length();
+        for(int i = 0; i < n; i++) {
+            charMap[s[i] - 'a'].push_back(i);
+        }
         
-        while (i < m && j < n) {
-            if (a[i] == b[j]) {
-                i++; j++;
-            } else {
-                i++;
+        int ans = words.size();
+        for(auto& word : words) {
+            int last = -1;
+            
+            for(char c : word) {
+                auto& cIndexes = charMap[c - 'a'];
+                auto it = upper_bound(cIndexes.begin(), cIndexes.end(), last);
+                if(it == cIndexes.end()) {
+                    ans--;
+                    break;
+                } else {
+                    last = *it;
+                }
             }
         }
         
-        return (j == n);
-    }
-public:
-    int numMatchingSubseq(string S, vector<string>& words) {
-        
-        int c = 0;
-        unordered_map<string, int>mp;
-        for (int i = 0; i < words.size(); i++)
-            mp[words[i]]++;
-            
-        for (auto it = mp.begin(); it != mp.end(); it++) {
-            if (isSubSeq(S, it->first))
-                c += it->second;
-        }
-        
-        return c;
+        return ans;
     }
 };
