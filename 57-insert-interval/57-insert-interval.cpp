6cf7ad51e9ecd30bc4f57a183d1c538bc6f8299e
Pratham Jain <83359099+iprathamjain@users.noreply.github.com>
2022-08-22 15:42:22 +0530

    Time: 13 ms (93.75%), Space: 17.1 MB (37.93%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,24 +1,38 @@
+/*
+    Given array of non-overlapping intervals & a new interval, insert & merge if necessary
+    Ex. intervals = [[1,3],[6,9]], newInterval = [2,5] -> [[1,5],[6,9]]
+
+    To merge: while intervals are still overlapping the new one, take the larger bounds
+
+    Time: O(n)
+    Space: O(n)
+*/
+
 class Solution {
 public:
-    
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& ni) {
-        intervals.push_back(ni);
-        if(intervals.size()<=1)
-            return intervals;
-        vector<vector<int>>v;
-        sort(intervals.begin(),intervals.end());
-        v.push_back(intervals[0]);
-        int j=0;
-        for(int i=1;i<intervals.size();i++)
-        {
-            if(intervals[i][0]<=v[j][1])
-                v[j][1]=max(intervals[i][1],v[j][1]);
-            else
-            {
-                v.push_back(intervals[i]);
-                j++;
-            }
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int i = 0;
+        int n = intervals.size();
+        
+        vector<vector<int>> result;
+        
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.push_back(intervals[i]);
+            i++;
         }
-        return v;
+        
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.push_back(newInterval);
+        
+        while (i < n) {
+            result.push_back(intervals[i]);
+            i++;
+        }
+        
+        return result;
     }
 };
