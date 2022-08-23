d35cb9befe46f653f250bfa67b89619cd2d84be1
Pratham Jain <83359099+iprathamjain@users.noreply.github.com>
2022-08-23 09:57:54 +0530

    Time: 8 ms (90.40%), Space: 8.3 MB (59.57%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.size()==0)
-            return 0;
-        unordered_set<char>st;
-        int ans = 1;
-        st.insert(s[0]);
-        int t = 1;
-        int in = 0;
-        int i =1;
-        st.insert(s[0]);
-        while(i<size(s)){
-            if(st.find(s[i])==st.end()){
-                t++;
-                st.insert(s[i]);
-                ans=max(ans,t);
-                i++;
-            }
-            else{
-                st.erase(s[in]);
-                t--;
-                in++;
-            }
+        int n = s.length();
+        int start = 0;
+        int ans = 0;
+        vector<int>prev(256,-1);
+        for(int end = 0;end<n;end++){
+            start = max(start,prev[s[end]]+1);
+            int maxend = end-start+1;
+            ans = max(ans,maxend);
+            prev[s[end]] = end;
         }
-        
         return ans;
+//         if(s.size()==0)
+//             return 0;
+//         unordered_set<char>st;
+//         int ans = 1;
+//         st.insert(s[0]);
+//         int t = 1;
+//         int in = 0;
+//         int i =1;
+//         st.insert(s[0]);
+//         while(i<size(s)){
+//             if(st.find(s[i])==st.end()){
+//                 t++;
+//                 st.insert(s[i]);
+//                 ans=max(ans,t);
+//                 i++;
+//             }
+//             else{
+//                 st.erase(s[in]);
+//                 t--;
+//                 in++;
+//             }
+//         }
+        
+//         return ans;
     }
 };
