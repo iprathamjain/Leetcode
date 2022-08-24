50847e5f67d59cf393be5f54a07b254bb5ebd8e7
Pratham Jain <83359099+iprathamjain@users.noreply.github.com>
2022-08-24 10:26:58 +0530

    Time: 59 ms (7.71%), Space: 5.7 MB (88.51%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
+    int const Max3PowerInt = 1162261467; // 3^19, 3^20 = 3486784401 > MaxInt32
+    int const MaxInt32 = 2147483647; // 2^31 - 1
     bool isPowerOfThree(int n) {
-        if(n&1==0 or n==0)
-            return false;
-        while(n%3==0){
-            n/=3;
-        }
-        if(n==1)
-            return 1;
-        return 0;
+        if (n <= 0 || n > Max3PowerInt) return false;
+        return Max3PowerInt % n == 0;
     }
 };
