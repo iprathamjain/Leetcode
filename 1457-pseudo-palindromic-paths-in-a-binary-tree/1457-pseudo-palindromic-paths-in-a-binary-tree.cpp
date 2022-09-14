c9dc56a1c4562b7de811bea69c5c728853a955c2
Pratham Jain <83359099+iprathamjain@users.noreply.github.com>
2022-09-14 08:50:25 +0530

    Time: 829 ms (23.18%), Space: 192.1 MB (39.26%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int res = 0;
+    unordered_map<int, int> m;
+    
+    void rec(TreeNode* root) {
+        if (!root)
+            return;
+        
+        m[root->val]++;
+        
+        if (!root->left && !root->right) {
+            int odd = 0;
+            for (auto a : m)
+                if (a.second % 2 == 1)
+                    odd++;
+
+            if (odd <= 1)
+                res++;
+        }
+        
+        rec(root->left);
+        rec(root->right);
+        m[root->val]--;
+    }
+    
+    int pseudoPalindromicPaths (TreeNode* root) {
+        rec(root);
+        return res;
+    }
+
+};
